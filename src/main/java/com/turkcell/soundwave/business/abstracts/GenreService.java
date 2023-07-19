86d605fd0939c4cfea1235502b8d9b06140cab1f
ismail <atac571@outlook.com>
2023-07-19 15:09:35 +0300

    genre service add

---
+++ src/main/java/com/turkcell/soundwave/business/abstracts/GenreService.java
@@ -0,0 +1,18 @@
+package com.turkcell.soundwave.business.abstracts;
+
+import com.turkcell.soundwave.business.dtos.genre.*;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface GenreService {
+    List<GetAllGenresResponse> getAll();
+
+    CreateGenreResponse add(CreateGenreRequest request);
+
+    void delete(UUID id);
+
+    UpdateGenreResponse update(UUID id, UpdateGenreRequest request);
+
+    GetGenreResponse getById(UUID id);
+}
