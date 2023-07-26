e0a6ec7ab2edfb5a01f5013a8ed4384e5d44cb9b
ismail <atac571@outlook.com>
2023-07-26 14:46:44 +0300

    edit application files

---
+++ src/test/java/com/atmosware/soundwave/SoundwaveApplicationTests.java
@@ -0,0 +1,15 @@
+package com.atmosware.soundwave;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SoundwaveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("Hello");
+	}
+
+}
